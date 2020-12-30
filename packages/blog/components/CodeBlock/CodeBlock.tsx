9fdd754059132365e7ee3aa996e3a9348f0389aa
Julio Soto <jsberlanga@gmail.com>
2020-12-30 20:52:34 +0100

    feat(blog): fontweight 400 roboto mono

--- packages/blog/components/CodeBlock/CodeBlock.tsx
+++ packages/blog/components/CodeBlock/CodeBlock.tsx
@@ -44,7 +44,7 @@ const CodeBlock = ({
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@500&&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400&&display=swap"
           rel="stylesheet"
         />
       </Head>
