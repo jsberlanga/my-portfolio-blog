6911e3b1e139dba7236d7f567d35db1041b29db9
Julio Soto <jsberlanga@gmail.com>
2020-12-30 20:54:08 +0100

    feat(components): update scrollprogress styles

--- packages/components/ScrollProgress/ScrollProgress.tsx
+++ packages/components/ScrollProgress/ScrollProgress.tsx
@@ -1,9 +1,8 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import { getMQ } from '@juliosoto/utils/styles';
 import {
   motion,
-  useSpring,
+  useMotionTemplate,
   useTransform,
   useViewportScroll,
 } from 'framer-motion';
@@ -11,53 +10,18 @@ import * as React from 'react';
 
 const styles = css`
   position: fixed;
-  top: 1rem;
-  right: 1rem;
-  width: 4rem;
-  height: 4rem;
-
-  ${getMQ('mobile')} {
-    display: none;
-  }
+  top: 0;
+  left: 0;
+  height: 10px;
+  background: linear-gradient(to left, var(--c-special-02), var(--c-special));
 `;
 
 export const ScrollProgress = () => {
-  const [isComplete, setIsComplete] = React.useState(false);
   const { scrollYProgress } = useViewportScroll();
-  const yRange = useTransform(scrollYProgress, [0, 0.9], [0, 1]);
-  const pathLength = useSpring(yRange, { stiffness: 400, damping: 90 });
+  const yRange = useTransform(scrollYProgress, [0, 1], [0, 100]);
+  const width = useMotionTemplate`${yRange}%`;
 
-  React.useEffect(() => yRange.onChange((v) => setIsComplete(v >= 1)), [
-    yRange,
-  ]);
-
-  return (
-    <svg className="progress-icon" viewBox="0 0 60 60" css={styles}>
-      <motion.path
-        fill="none"
-        strokeWidth="3"
-        stroke="var(--c-text)"
-        strokeDasharray="0 1"
-        d="M 0, 20 a 20, 20 0 1,0 40,0 a 20, 20 0 1,0 -40,0"
-        style={{
-          pathLength,
-          rotate: 90,
-          translateX: 5,
-          translateY: 5,
-          scaleX: -1,
-        }}
-      />
-      <motion.path
-        fill="none"
-        strokeWidth="3"
-        stroke="var(--c-text)"
-        d="M14,26 L 22,33 L 35,16"
-        initial={false}
-        strokeDasharray="0 1"
-        animate={{ pathLength: isComplete ? 1 : 0 }}
-      />
-    </svg>
-  );
+  return <motion.div css={styles} style={{ width }} />;
 };
 
 export default ScrollProgress;
