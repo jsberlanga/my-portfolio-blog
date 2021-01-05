439469049f61b4b0cecb861c2f8e2197436ed181
Julio Soto <jsberlanga@gmail.com>
2021-01-05 13:30:22 +0100

    fix(blog):remove image loader from  nextjs config

--- packages/blog/next.config.js
+++ packages/blog/next.config.js
@@ -18,9 +18,4 @@ module.exports = withPlugins([withMDX, withTM], {
     MONGODB_URI: process.env.MONGODB_URI,
     BUTTONDOWN_AUTH_TOKEN: process.env.BUTTONDOWN_AUTH_TOKEN,
   },
-  images: {
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    domains: ['images.ctfassets.net'],
-    loader: 'default',
-  },
 });
