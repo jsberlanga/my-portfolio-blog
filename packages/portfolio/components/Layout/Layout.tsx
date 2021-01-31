0c9072a35a0bab0271a0f69812ca257d8367cc13
Julio Soto <jsberlanga@gmail.com>
2021-01-31 01:31:43 +0100

    feat: no initial loading

--- packages/portfolio/components/Layout/Layout.tsx
+++ packages/portfolio/components/Layout/Layout.tsx
@@ -1,24 +1,10 @@
 import * as React from 'react';
 import Layout from '@juliosoto/components/Layout';
-import Loading from '../Loading';
 import PortfolioFooter from '../Footer';
 import PortfolioNavbar from '../Navbar';
 import { AnimatePresence } from 'framer-motion';
 
 const PortfolioLayout: React.FC = ({ children }) => {
-  const [initialLoading, setInitialLoading] = React.useState(true);
-
-  React.useEffect(() => {
-    setTimeout(() => setInitialLoading(false), 2000);
-  }, []);
-
-  if (initialLoading) {
-    return (
-      <AnimatePresence>
-        <Loading key="loading" />;
-      </AnimatePresence>
-    );
-  }
   return (
     <AnimatePresence>
       <Layout components={{ Footer: PortfolioFooter, Navbar: PortfolioNavbar }}>
